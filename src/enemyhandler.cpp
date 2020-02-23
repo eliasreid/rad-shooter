2cbd90ed7238eaa5b31596647d23f09bb00135fa
Elias Reid <elre233@gmail.com>
2020-02-22 22:28:20 -0400

    fixed path

--- src/enemyhandler.cpp
+++ src/enemyhandler.cpp
@@ -144,7 +144,7 @@ void EnemyHandler::SpawnEnemy(Enemy::TYPE enemy_type, float initial_speed){
     break;
   }
 
-  enemies_.push_back(std::make_shared<Enemy>(enemy_type, renderer_, window_, "../rad-shooter-POC/assets/enemy.png", spawn_rect,vel));
+  enemies_.push_back(std::make_shared<Enemy>(enemy_type, renderer_, window_, "../rad-shooter/assets/enemy.png", spawn_rect,vel));
 }
 
 /*
